bd0675170d0f6f749d0214787a389ed49a9cabd0
Hendra <hen.hendra1806@gmail.com>
2023-08-01 16:03:47 +0700

    feature : global scss

--- garudaku_frontend/src/main.js
+++ garudaku_frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './stores';
+import './assets/style.scss'
 
 Vue.config.productionTip = false;
 
